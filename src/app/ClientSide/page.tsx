1fb33ab7874e05d29c4d48a7bc7c778c52389ce1
ABIHAAHMED4262 <abihaahmed413@gmail.com>
2024-12-25 23:13:36 +0500

    Changes in next.config.ts

--- src/app/ClientSide/page.tsx
+++ src/app/ClientSide/page.tsx
@@ -21,6 +21,7 @@ const ClientSide: React.FC = () => {
         setProducts(data);
         setLoading(false);
       } catch (err: unknown) {
+        console.error("Error fetching products:", err);
         if (err instanceof Error) {
           setError(err.message);
         } else {
@@ -34,7 +35,11 @@ const ClientSide: React.FC = () => {
   }, []);
 
   if (loading) {
-    return <div className="container mx-auto p-4">Loading...</div>;
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-16 h-16 border-4 border-blue-950 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
   }
 
   if (error) {
@@ -54,21 +59,21 @@ const ClientSide: React.FC = () => {
               key={product.id}
               className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center border hover:shadow-lg transition duration-300"
             >
-              <Image
-                src={product.image}
-                alt={product.title}
-                width={200} // Set appropriate width
-                height={160} // Set appropriate height
-                className="object-contain mb-4"
-              />
-
+              <div className="relative w-full h-40">
+                <Image
+                  src={product.image}
+                  alt={product.title}
+                  layout="fill"
+                  objectFit="contain"
+                />
+              </div>
               <h3 className="font-semibold text-lg mb-2 text-center text-blue-950">
                 {product.title}
               </h3>
               <p className="text-sm text-blue-950 mb-4 text-center">
                 ${product.price.toFixed(2)}
               </p>
-              <button className="bg-blue-950 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
+              <button className="bg-blue-950 text-white px-4 py-2 rounded hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 transition duration-300">
                 Add to Cart
               </button>
             </div>
--- src/types/types.ts
+++ src/types/types.ts
@@ -9,6 +9,8 @@ export interface Book {
     id: number;
     title: string;
     price: number;
+    description: string;
+    category: string;
     image: string;
   }
   
